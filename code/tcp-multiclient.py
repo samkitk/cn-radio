cf06eba3d196dbc19b30786bf996ff04f479bf73
Samkit <28357583+samkitk@users.noreply.github.com>
2022-05-07 11:01:18 +0530

    feat:tcp dict transfer and print done

--- code/tcp-multiclient.py
+++ code/tcp-multiclient.py
@@ -1,7 +1,17 @@
 import socket
+import pprint
+import pickle
 
 HOST = socket.gethostname()  # The server's hostname or IP address
-PORT = 8082  # The port used by the server
+PORT = 8079  # The port used by the server
+
+# def UserInputMenu():
+#     user_input = input()
+#     if(user_input=="")
+
+
+
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
@@ -10,3 +20,21 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # sep = "\0"
     string = str(num) 
     s.send(string.encode())
+    data = s.recv(4096) ##randomly taken 4096
+    station_list = pickle.loads(data)
+    # print(type(station_list))
+    print("======================")
+    print("Welcome to",station_list['site_info']['site_name'])
+    print("======================")
+    print("We have",station_list['site_info']['radio_stn_count'],"station(s) available for you!")
+    print(station_list['station1']['radio_stn_number'],"-",station_list['station1']['radio_stn_name'])
+    print("Address:",station_list['station1']['multicast_address'])
+    print("Info Port:",station_list['station1']['info_port'])
+    print("Data Port:",station_list['station1']['data_port'])
+    print("BitRate:",station_list['station1']['bit_rate'])
+    print("----------------------")
+    print(station_list['station2']['radio_stn_number'],"-",station_list['station2']['radio_stn_name'])
+    print("Address:",station_list['station2']['multicast_address'])
+    print("Info Port:",station_list['station2']['info_port'])
+    print("Data Port:",station_list['station2']['data_port'])
+    print("BitRate:",station_list['station2']['bit_rate'])
--- code/tcp-multiserver.py
+++ code/tcp-multiserver.py
@@ -20,9 +20,50 @@
 #!/usr/bin/python           # This is server.py file                                                                                                                                                                           
 
 import socket               # Import socket module
-import _thread
+import _thread, threading
+import pickle
 
-def on_new_client(clientsocket,addr):
+
+site_info = {
+    "type" : 10,
+    "site_name_size" : 5,
+    "site_name" : "RadioSite",
+    "site_desc_size" : 15,
+    "site_desc" : "This is RadioSite",
+    "radio_stn_count" : 2
+}
+
+station1 = {
+    "radio_stn_number" : 1,
+    "radio_stn_name_size" : 10,
+    "radio_stn_name" : "Red FM",
+    "multicast_address" : "239.192.1.1",
+    "data_port": 8000,
+    "info_port" : 8001,
+    "bit_rate" : 44100
+}
+
+station2 = {
+    "radio_stn_number" : 2,
+    "radio_stn_name_size" : 10,
+    "radio_stn_name" : "Love FM",
+    "multicast_address" : "239.192.1.2",
+    "data_port" : 8000,
+    "info_port" : 8001,
+    "bit_rate"  : 44100
+}
+
+radio_stn_info = {
+    "site_info": site_info,
+    "station1" : station1,
+    "station2" : station2
+}
+
+pickled_radio_stn_info = pickle.dumps(radio_stn_info)
+# pickled_site_info = pickle.dumps(site_info)
+
+
+def on_new_client(clientsocket,addr,pickled_radio_stn_info):
     # print('Got connection from', addr)
     msg = clientsocket.recv(1024)
     temp = msg.decode()
@@ -30,6 +71,10 @@ def on_new_client(clientsocket,addr):
     # client_msg = int(temp)
     if(temp=="1"):
         print("Succesfully Received Handshake from", addr)
+        print("Now sending Station List to", addr)
+        clientsocket.send(pickled_radio_stn_info)
+    print("Sent List")
+    clientsocket.close()
     #do some checks and if msg == someWeirdSignal: break:
     # print(addr, ' >> ', msg)
     # msg = raw_input('SERVER >> ')
@@ -37,11 +82,10 @@ def on_new_client(clientsocket,addr):
     # num = 1
     # sep = "\0"
     # clientsocket.send((str(num)+sep).encode())
-    clientsocket.close()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
 host = socket.gethostname() # Get local machine name
-port = 8082              # Reserve a port for your service.
+port = 8079              # Reserve a port for your service.
 
 print('Server started!')
 print('Waiting for clients...')
@@ -51,11 +95,11 @@ s.listen(5)                 # Now wait for client connection.
 
 while True:
    c, addr = s.accept()     # Establish connection with client.
-   _thread.start_new_thread(on_new_client,(c,addr))
-   x = input()
-   if(x=="a"):
-       s.close()
-       break
+   _thread.start_new_thread(on_new_client,(c,addr,pickled_radio_stn_info))
+#    x = input()
+#    if(x=="a"):
+#        s.close()
+#        break
    #Note it's (addr,) not (addr) because second parameter is a tuple
    #Edit: (c,addr)
    #that's how you pass arguments to functions when creating new threads using thread module.
