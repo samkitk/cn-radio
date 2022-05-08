14fbb9fbb18abd2767b16dbeb8799eb3ee3d5d75
malav312 <malavdoshi312@gmailcom>
2022-05-08 14:57:21 +0530

    Integrated

--- code/server.py
+++ code/server.py
@@ -1,4 +1,4 @@
-import socket  # Import socket module
+import socket 
 import _thread, threading
 import pickle
 from xmlrpc import client
@@ -7,6 +7,9 @@ import wave
 import time
 import math
 import struct 
+import os
+from tinytag import TinyTag
+import numpy as np
 
 site_info = {
     "type": 10,
@@ -22,18 +25,18 @@ station1 = {
     "radio_stn_name_size": 10,
     "radio_stn_name": "Red FM",
     "multicast_address": "239.192.1.1",
-    "data_port": 8000,
-    "info_port": 8001,
+    "data_port": 5007,
+    "info_port": 5432,
     "bit_rate": 44100,
 }
 
 station2 = {
     "radio_stn_number": 2,
     "radio_stn_name_size": 10,
-    "radio_stn_name": "Love FM",
+    "radio_stn_name": "Blue FM",
     "multicast_address": "239.192.1.2",
-    "data_port": 8000,
-    "info_port": 8001,
+    "data_port": 5007,
+    "info_port": 5432,
     "bit_rate": 44100,
 }
 
@@ -52,78 +55,198 @@ pickled_radio_stn_info = pickle.dumps(radio_stn_info)
 # Edit: (c,addr)
 # that's how you pass arguments to functions when creating new threads using thread module.
 
-#-------------UDP SERVER SOCKET----------------
-### STATION 1
-MCAST_PORT = 8000
-MCAST_GRP = '239.192.1.1'
+#-------------UDP SERVER SOCKET----------------\
+global path_Station1
+path_Station1 = os.listdir("data/Station_1")
+path_Station2 = os.listdir("data/Station_2")
 ttl = struct.pack('b', 2)
+MCAST_PORT = 5007 #DataPort
+
 IS_ALL_GROUPS = True
 
-def audio_stream_UDP():
-    BUFF_SIZE = 65536
-    audio_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    audio_server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
-    CHUNK = 1024
-    print("Playing Wav File")
-    wf = wave.open("data/10MB.wav")
-    p = pyaudio.PyAudio()   
-    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                    channels=wf.getnchannels(),
-                    rate=wf.getframerate(),
-                    input=True,
-                    frames_per_buffer=CHUNK)
+def station_1():
+    #=========== STATION 1 ================
+    MCAST_GRP_STATION1 = '239.192.1.1'
 
-    data = None
-    sample_rate = wf.getframerate()
-    while True:
-        audio_server_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
-        DATA_SIZE = math.ceil(wf.getnframes()/CHUNK)
-        DATA_SIZE = str(DATA_SIZE).encode()
-        print('[Sending data size]...', wf.getnframes()/sample_rate)
-        audio_server_socket.sendto(DATA_SIZE, (MCAST_GRP, MCAST_PORT))
-        cnt = 0
-        while True:
-            data = wf.readframes(CHUNK)
-            audio_server_socket.sendto(data, (MCAST_GRP, MCAST_PORT))
-            time.sleep(0.001)
-            print(cnt)
-            if cnt > (wf.getnframes()/CHUNK):
+    #---------- MULTIMEDIA MULTICAST-----
+    def audio_stream_UDP_Station1():
+        BUFF_SIZE = 65536
+        audio_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        audio_server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
+        CHUNK = 10240
+        for i in path_Station1:
+            wf = wave.open("data/Station_1/" + i)
+            p = pyaudio.PyAudio()   
+            stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                            channels=wf.getnchannels(),
+                            rate=wf.getframerate(),
+                            input=True,
+                            frames_per_buffer=CHUNK)
+            data = None
+            sample_rate = wf.getframerate()
+            print(sample_rate)
+            while True:
+                audio_server_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+                DATA_SIZE = math.ceil(wf.getnframes()/CHUNK)
+                DATA_SIZE = str(DATA_SIZE).encode()
+                # DURATION = DATA_SIZE*CHUNK/44100
+
+                # print('[Sending data size]...', wf.getnframes()/sample_rate)
+                audio_server_socket.sendto(DATA_SIZE, (MCAST_GRP_STATION1, MCAST_PORT))
+                cnt = 0
+                while True:
+                    data = wf.readframes(CHUNK)
+                    audio_server_socket.sendto(data, (MCAST_GRP_STATION1, MCAST_PORT))
+                    time.sleep(0.005)
+                    print(cnt)
+                    if cnt > (wf.getnframes()/CHUNK):
+                        break
+                    cnt += 1
                 break
-            cnt += 1
-        break
-    print('SENT...')
+            print('SENT...')
 
-# audio_stream_UDP()
-t2 = threading.Thread(target=audio_stream_UDP, args=())
-t2.start()
+    #----------INFORMATION MULTICAST-----
+
+    MCAST_INFOPORT_S1 = 5007
+    def information_stream_Station1():
+        # print("Hello")
+        info_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        path_Station1 = os.listdir("data/Station_1")
+        n=len(path_Station1)
+        path_Station1 = np.array(path_Station1)
+        for x in range(len(path_Station1)):
+            i= TinyTag.get("data/Station_1/" + path_Station1[x])
+            title = path_Station1[x]
+            size_title = len(title)        
+            filesize = i.filesize
+            duration = i.duration
+            if(x<(len(path_Station1)-1)) :
+                k= TinyTag.get("data/Station_1/" + path_Station1[x+1])
+                next_song_title = path_Station1[x+1]
+                next_song_size = len(next_song_title)            
+                song_info = {"Song Name Size" : size_title, "title": title, "Filesize" : filesize,  "Next Song Size" : next_song_size, "Next Song Title" : next_song_title}
+            else:
+                song_info = {"Song Name Size" : size_title, "title": title, "Filesize" : filesize }
+            while(duration>0):
+                song_info.update({'Time Remaining': duration})
+                string = pickle.dumps(song_info)
+                info_server_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+                info_server_socket.sendto(string, (MCAST_GRP_STATION1, MCAST_INFOPORT_S1))
+                time.sleep(1)
+                duration = duration-1  
+                print("Information sent")        
+            
+
+    #-----------THREADING OF STATION 1 PROCESSES-----
+    taudio_S1= threading.Thread(target=audio_stream_UDP_Station1, args=())
+    tinfo_S1= threading.Thread(target=information_stream_Station1, args=())
+    taudio_S1.start()
+    tinfo_S1.start()
+    print("Station 1 Running")
 
 
+def station_2():
+    #=========== STATION 2 ============
+    MCAST_GRP_STATION2 = '239.192.1.2'
+
+    #-----------AUDIO STREAM OF STATION 2------
+    def audio_stream_UDP_Station2():
+        BUFF_SIZE = 65536
+        audio_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        audio_server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
+        CHUNK = 10240
+        for i in path_Station2:
+            wf = wave.open("data/Station_2/" + i)
+            p = pyaudio.PyAudio()   
+            stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                            channels=wf.getnchannels(),
+                            rate=wf.getframerate(),
+                            input=True,
+                            frames_per_buffer=CHUNK)
+            data = None
+            sample_rate = wf.getframerate()
+            print(sample_rate)
+            while True:
+                audio_server_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+                DATA_SIZE = math.ceil(wf.getnframes()/CHUNK)
+                DATA_SIZE = str(DATA_SIZE).encode()
+
+                print('[Sending data size]...', wf.getnframes()/sample_rate)
+                audio_server_socket.sendto(DATA_SIZE, (MCAST_GRP_STATION2, MCAST_PORT))
+                cnt = 0
+                while True:
+                    data = wf.readframes(CHUNK)
+                    audio_server_socket.sendto(data, (MCAST_GRP_STATION2, MCAST_PORT))
+                    time.sleep(0.005)
+                    print(cnt)
+                    if cnt > (wf.getnframes()/CHUNK):
+                        break
+                    cnt += 1
+                break
+            print('SENT...')
+
+
+    #----------INFORMATION STREAM---------------
+
+    MCAST_INFOPORT_S2 = 5007
+    def information_stream_Station2():
+        # print("Hello")
+        info_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        path_Station2 = os.listdir("data/Station_2")
+        n=len(path_Station2)
+        path_Station2 = np.array(path_Station2)
+        for x in range(len(path_Station2)):
+            i= TinyTag.get("data/Station_2/" + path_Station2[x])
+            title = path_Station2[x]
+            size_title = len(title)        
+            filesize = i.filesize
+            duration = i.duration
+            if(x<(len(path_Station2)-1)) :
+                k= TinyTag.get("data/Station_2/" + path_Station2[x+1])
+                next_song_title = path_Station2[x+1]
+                next_song_size = len(next_song_title)            
+                song_info = {"Song Name Size" : size_title, "title": title, "Filesize" : filesize,  "Next Song Size" : next_song_size, "Next Song Title" : next_song_title}
+            else:
+                song_info = {"Song Name Size" : size_title, "title": title, "Filesize" : filesize }
+            while(duration>0):
+                song_info.update({'Time Remaining': duration})
+                string = pickle.dumps(song_info)
+                info_server_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+                info_server_socket.sendto(string, (MCAST_GRP_STATION2, MCAST_INFOPORT_S2))
+                time.sleep(1)
+                duration = duration-1  
+                print("Information sent")        
+
+    #--------THREADING OF STATION 1 PROCESSES-----          
+    taudio_S2= threading.Thread(target=audio_stream_UDP_Station2, args=())
+    tinfo_S2= threading.Thread(target=information_stream_Station2, args=())
+    taudio_S2.start()
+    tinfo_S2.start()
+    print("Station 2 Running")
+    
+
+t_s1=threading.Thread(target=station_1,args=())
+t_s2=threading.Thread(target=station_2,args=())
+
+t_s1.start()
+t_s2.start()
+    
 
 def on_new_client(clientsocket, addr, pickled_radio_stn_info):
-    # print('Got connection from', addr)
     msg = clientsocket.recv(1024)
     temp = msg.decode()
-    # print("----",temp,"----")
-    # client_msg = int(temp)
     if temp == "1":
         print("Succesfully Received Handshake from", addr)
         print("Now sending Station List to", addr)
         clientsocket.send(pickled_radio_stn_info)
     print("Sent List")
     clientsocket.close()
-    # do some checks and if msg == someWeirdSignal: break:
-    # print(addr, ' >> ', msg)
-    # msg = raw_input('SERVER >> ')
-    # Maybe some code to compute the last digit of PI, play game or anything else can go here and when you are done.
-    # num = 1
-    # sep = "\0"
-    # clientsocket.send((str(num)+sep).encode())
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 host = socket.gethostname()  # Get local machine name
-port = 8095  # Reserve a port for your service.
+port = 3003  # Reserve a port for your service.
 
 print("Server started!")
 print("Waiting for clients...")
@@ -134,3 +257,4 @@ while True:
     c, addr = s.accept()  # Establish connection with client.
     t1=threading.Thread(target=on_new_client,args=(c, addr, pickled_radio_stn_info))
     t1.start()
+
